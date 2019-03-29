19f1764d5ecab08c916da8cbb83523d4c2d00336
StanisLove Sid <joingamings@gmail.com>
2019-03-29 14:25:05 +0200

    if no previous test group

--- app/controllers/live_tests_controller.rb
+++ app/controllers/live_tests_controller.rb
@@ -9,15 +9,17 @@ class LiveTestsController < ApplicationController
     @a_z = ('a'..'z').to_a
     @test_group = TestGroup.find(@test.test_group_id)
     @subject = Subject.find(@test_group.subject_id)
-    @previous_test_group = @subject.test_groups.where(["id < ?", @test_group.id]).last
-    @previous_test_group_score = @previous_test_group.score
-    @user_previous_test_group_solutions = current_user.solutions.includes(:test).where(tests: { test_group_id: @previous_test_group.id})
-    @previous_test_group_passed = @previous_test_group.passed?(@previous_test_group.percents @user_previous_test_group_solutions.map(&:total_score).sum)
     @previous = @test_group.tests.where(["id < ?", @test.id]).last
+    @previous_test_group = @subject.test_groups.where(["id < ?", @test_group.id]).last
     @next = @test_group.tests.where(["id > ?", @test.id]).first
-    unless @previous_test_group_passed
-      redirect_back(fallback_location: live_test_group_path(@previous_test_group))
-      flash[:alert] = "To access next group of tests, firstly you have to pass previous"
+    unless @previous_test_group.nil?
+      @previous_test_group_score = @previous_test_group.nil? ? 0 : @previous_test_group.score
+      @user_previous_test_group_solutions = current_user.solutions.includes(:test).where(tests: { test_group_id: @previous_test_group.id})
+      @previous_test_group_passed = @previous_test_group.passed?(@previous_test_group.percents @user_previous_test_group_solutions.map(&:total_score).sum)
+      unless @previous_test_group_passed
+        redirect_back(fallback_location: live_test_group_path(@previous_test_group))
+        flash[:alert] = "To access next group of tests, firstly you have to pass previous"
+      end
     end
     unless @previous.nil?
       previous_soulution = Solution.find_by(user_id: current_user.id, test_id: @previous.id)
