c74a163c1929b7287bd9250437b0f1d618894471
StanisLove Sid <joingamings@gmail.com>
2019-03-31 14:14:24 +0300

    fixed redirection

--- app/controllers/live_tests_controller.rb
+++ app/controllers/live_tests_controller.rb
@@ -12,22 +12,21 @@ class LiveTestsController < ApplicationController
     @previous = @test_group.tests.where(["id < ?", @test.id]).last
     @previous_test_group = @subject.test_groups.where(["id < ?", @test_group.id]).last
     @next = @test_group.tests.where(["id > ?", @test.id]).first
-    unless @previous_test_group.nil?
-      @previous_test_group_score = @previous_test_group.nil? ? 0 : @previous_test_group.score
-      @user_previous_test_group_solutions = current_user.solutions.includes(:test).where(tests: { test_group_id: @previous_test_group.id})
-      @previous_test_group_passed = @previous_test_group.passed?(@previous_test_group.percents @user_previous_test_group_solutions.map(&:total_score).sum)
-      unless @previous_test_group_passed
-        redirect_back(fallback_location: live_test_group_path(@previous_test_group))
-        flash[:alert] = "To access next group of tests, firstly you have to pass previous"
-      end
-    end
-    unless @previous.nil?
+    if !(@previous.nil?)
       previous_soulution = Solution.find_by(user_id: current_user.id, test_id: @previous.id)
       total_score = previous_soulution.nil? ? 0 : previous_soulution.total_score
       previous_passed = @previous.passed? (@previous.percents total_score)
       unless previous_passed
         redirect_back(fallback_location: live_test_path(@previous))
         flash[:alert] = "To access next test, firstly you have to pass previous"
+      end
+    elsif !(@previous_test_group.nil?)
+      @previous_test_group_score = @previous_test_group.nil? ? 0 : @previous_test_group.score
+      @user_previous_test_group_solutions = current_user.solutions.includes(:test).where(tests: { test_group_id: @previous_test_group.id})
+      @previous_test_group_passed = @previous_test_group.passed?(@previous_test_group.percents @user_previous_test_group_solutions.map(&:total_score).sum)
+      unless @previous_test_group_passed
+        redirect_back(fallback_location: live_test_group_path(@previous_test_group))
+        flash[:alert] = "To access next group of tests, firstly you have to pass previous"
       end
     end
     current_solution = current_user.solutions.where(["test_id = ?", @test.id]).first
